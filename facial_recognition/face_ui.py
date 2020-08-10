7f9c96530140c74c4a19092383e3eb0029dcb021
Kant Sunthad <kant@sennalabs.com>
2020-08-10 17:02:19 +0700

    Add tkinter

---
+++ facial_recognition/face_ui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from PIL import Image
+from PIL import ImageTk
+from tkinter import filedialog
+import cv2
+
+
+gui = Tk(className='Python Examples - Window Size')
+# set window size
+gui.geometry("1350x800")
+
+gui.mainloop() 
